00e0a81546f2c977b551bc10baf9bd0d75e59393
Сергей <parenek.serega@gmail.com>
2021-11-07 11:09:07 +0300

    os_info.txt updated

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import sys
 info = "OS Info is \n{}\n\nPython version is {} {}".format(platform.uname(), sys.version, platform.architecture())
 print(info)
 
-with open("Solutions/1/os_info.txt", 'w') as ff:
+with open("os_info.txt", 'w') as ff:
     ff.write(info)
